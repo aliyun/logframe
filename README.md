f8450fc5a5478e607d808cf7ecf43c36ff918be3
马云雷 <yunlei.mayl@alibaba-inc.com>
2023-11-01 14:02:59 +0800

    add sample

--- README.md
+++ README.md
@@ -14,5 +14,10 @@ beginTime = endTime- 600
 sls = LogClient(endpoint = endpoint, accessKeyId = accessId, accessKey= accessKey)
 res = sls.get(project=project,logstore='http',fromTime=beginTime,toTime=endTime,query='* | select count(1) as pv, (response_code) as response_code group by response_code')
 print (res)
+res = sls.get(project=project,logstore='presto_http',fromTime=beginTime,toTime=endTime,query='* | insert into tmp_table(b,a) select count(1) as pv, cast(response_code as varchar) as response_code group by response_code')
+print (res)
+res = sls.get(project=project,logstore='presto_http',fromTime=beginTime,toTime=endTime,query='response_code:200',keys=['client_address','method','uri','response_code'],fullData=True,timeoutSec=5)
+print (res)
+print (res.describe())
 
 ```
--- sample.py
+++ sample.py
@@ -4,14 +4,16 @@ import time
 accessId  = ""
 accessKey = ""
 endpoint = 'cn-hangzhou.log.aliyuncs.com'
-project = 'ali-cn-xxxx'
+project = 'ali-cn-xxx'
+logstore = 'http'
 endTime = (int)(time.time())
 beginTime = endTime- 1
 
 sls = LogClient(endpoint = endpoint, accessKeyId = accessId, accessKey= accessKey)
-res = sls.get(project=project,logstore='presto_http',fromTime=beginTime,toTime=endTime,query='* | select count(1) as pv, (response_code) as response_code group by response_code')
+res = sls.get(project=project,logstore=logstore,fromTime=beginTime,toTime=endTime,query='* | select count(1) as pv, (response_code) as response_code group by response_code')
 print (res)
-res = sls.get(project=project,logstore='presto_http',fromTime=beginTime,toTime=endTime,query='* | insert into tmp_table(b,a) select count(1) as pv, cast(response_code as varchar) as response_code group by response_code')
+res = sls.get(project=project,logstore=logstore,fromTime=beginTime,toTime=endTime,query='* | insert into tmp_table(b,a) select count(1) as pv, cast(response_code as varchar) as response_code group by response_code')
 print (res)
-res = sls.get(project=project,logstore='presto_http',fromTime=beginTime,toTime=endTime,query='response_code:200',keys=['client_address','method','uri','response_code'],fullData=True,timeoutSec=5)
+res = sls.get(project=project,logstore=logstore,fromTime=beginTime,toTime=endTime,query='response_code:200',keys=['client_address','method','uri','response_code'],fullData=True,timeoutSec=5)
 print (res)
+print (res.describe())
